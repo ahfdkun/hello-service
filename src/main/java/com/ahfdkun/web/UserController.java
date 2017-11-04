e5cf0f584c28c02522def43d2475301e99b12fcc
Administrator <Administrator@VBH8YFQJAWZA3G3>
2017-11-04 20:45:44 +0800

    请求合并

--- src/main/java/com/ahfdkun/web/UserController.java
+++ src/main/java/com/ahfdkun/web/UserController.java
@@ -1,5 +1,9 @@
 package com.ahfdkun.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
@@ -8,7 +12,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ahfdkun.domain.User;
 
 @RestController
 public class UserController {
@@ -24,12 +31,24 @@ public class UserController {
 		logger.info("/users/" + id + ", host:" + instance.getHost() + ", service_id:" + instance.getServiceId());
 		return "getUser id: " + id;
 	}
-	
+
 	@RequestMapping(value = "/users", method = RequestMethod.POST)
 	public String postUser(@RequestBody Long id) {
 		ServiceInstance instance = client.getLocalServiceInstance();
 		logger.info("/users/" + id + ", host:" + instance.getHost() + ", service_id:" + instance.getServiceId());
 		return "postUser id: " + id;
+	}
+
+	@RequestMapping(value = "/users1", method = RequestMethod.GET)
+	public List<User> getUser1(String ids) {
+		ServiceInstance instance = client.getLocalServiceInstance();
+		logger.info("/users1?ids" + ids + ", host:" + instance.getHost() + ", service_id:" + instance.getServiceId());
+		String[] idsAttr = StringUtils.split(ids, ",");
+		List<User> users = new ArrayList<>();
+		for (String id : idsAttr) {
+			users.add(new User(Long.valueOf(id), "name" + id));
+		}
+		return users;
 	}
 
 }
