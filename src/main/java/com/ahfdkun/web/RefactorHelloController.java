f07b2761ee491daafbecadcbfb2411075da2b11b
ahfdkun <785440131@qq.com>
2017-11-27 20:10:14 +0800

    Feign参数绑定和继承特性

--- src/main/java/com/ahfdkun/web/RefactorHelloController.java
+++ src/main/java/com/ahfdkun/web/RefactorHelloController.java
@@ -6,47 +6,35 @@ import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ahfdkun.domain.User;
-import com.ahfdkun.domain.User2;
+import com.ahfdkun.api.service.HelloService;
+import com.ahfdkun.api.dto.User2;
 
 @RestController
-public class HelloController {
+public class RefactorHelloController implements HelloService {
 
-	private final Logger logger = Logger.getLogger(HelloController.class);
+	private final Logger logger = Logger.getLogger(RefactorHelloController.class);
 
 	@Autowired
 	private DiscoveryClient client; // 获取服务的信息
-
-	@RequestMapping(value = "/hello", method = RequestMethod.GET)
-	public String hello() {
-		ServiceInstance instance = client.getLocalServiceInstance();
-		logger.info("/hello, host:" + instance.getHost() + ", service_id:" + instance.getServiceId());
-		return "Hello World";
-	}
 	
-	@RequestMapping(value = "/hello1", method = RequestMethod.GET)
 	public String hello(@RequestParam String name) {
 		ServiceInstance instance = client.getLocalServiceInstance();
-		logger.info("/hello1, host:" + instance.getHost() + ", service_id:" + instance.getServiceId());
+		logger.info("/hello4, host:" + instance.getHost() + ", service_id:" + instance.getServiceId());
 		return "Hello " + name;
 	}
 	
-	@RequestMapping(value = "/hello2", method = RequestMethod.GET)
 	public User2 hello(@RequestHeader String name, @RequestHeader Integer age) {
 		ServiceInstance instance = client.getLocalServiceInstance();
-		logger.info("/hello2, host:" + instance.getHost() + ", service_id:" + instance.getServiceId());
+		logger.info("/hello5, host:" + instance.getHost() + ", service_id:" + instance.getServiceId());
 		return new User2(name, age);
 	}
 	
-	@RequestMapping(value = "/hello3", method = RequestMethod.POST)
 	public String hello(@RequestBody User2 user2) {
 		ServiceInstance instance = client.getLocalServiceInstance();
-		logger.info("/hello3, host:" + instance.getHost() + ", service_id:" + instance.getServiceId());
+		logger.info("/hello6, host:" + instance.getHost() + ", service_id:" + instance.getServiceId());
 		return "Hello " + user2.getName() + ", " + user2.getAge();
 	}
 
