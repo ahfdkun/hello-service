84d878e655e0869767c456ebf8069536d6b840c7
ahfdkun <785440131@qq.com>
2017-12-05 20:00:40 +0800

    Feign重试机制

--- src/main/java/com/ahfdkun/web/HelloController.java
+++ src/main/java/com/ahfdkun/web/HelloController.java
@@ -1,5 +1,6 @@
 package com.ahfdkun.web;
 
+import org.apache.commons.lang.math.RandomUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
@@ -23,26 +24,29 @@ public class HelloController {
 	private DiscoveryClient client; // 获取服务的信息
 
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
-	public String hello() {
+	public String hello() throws Exception {
 		ServiceInstance instance = client.getLocalServiceInstance();
+		int sleepTime = RandomUtils.nextInt(60000);
+		logger.info("sleepTime: " + sleepTime);
+		Thread.sleep(sleepTime);
 		logger.info("/hello, host:" + instance.getHost() + ", service_id:" + instance.getServiceId());
 		return "Hello World";
 	}
-	
+
 	@RequestMapping(value = "/hello1", method = RequestMethod.GET)
 	public String hello(@RequestParam String name) {
 		ServiceInstance instance = client.getLocalServiceInstance();
 		logger.info("/hello1, host:" + instance.getHost() + ", service_id:" + instance.getServiceId());
 		return "Hello " + name;
 	}
-	
+
 	@RequestMapping(value = "/hello2", method = RequestMethod.GET)
 	public User2 hello(@RequestHeader String name, @RequestHeader Integer age) {
 		ServiceInstance instance = client.getLocalServiceInstance();
 		logger.info("/hello2, host:" + instance.getHost() + ", service_id:" + instance.getServiceId());
 		return new User2(name, age);
 	}
-	
+
 	@RequestMapping(value = "/hello3", method = RequestMethod.POST)
 	public String hello(@RequestBody User2 user2) {
 		ServiceInstance instance = client.getLocalServiceInstance();
